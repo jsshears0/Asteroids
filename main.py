a6e1cf6c952c736edff9d528185cd413fd419092
Jsshears0 <jsshears2@gmail.com>
2025-04-06 17:12:17 -0400

    Added Player sprite / corrected white space

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting Asteroids!") 
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-running = True
-fps = pygame.time.Clock()
-dt = 0
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    fps = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    screen.fill((0, 0, 0))
-        #setting screen color.
-    dt = fps.tick(60) / 1000
-        #setting framerate to 60fps
-        #print(f"DT: {dt}") #checking frame rate
-    pygame.display.flip()
+        screen.fill((0, 0, 0))
+            #setting screen color.
+        player.draw(screen)
+            #creating player sprite
+        dt = fps.tick(60) / 1000
+            #setting framerate to 60fps
+            #print(f"DT: {dt}") #checking frame rate
+        
+        pygame.display.flip()
 
-pygame.quit()
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
