ce2145ca01a409dd373284a3017db24fd9d7991f
Jsshears0 <jsshears2@gmail.com>
2025-04-06 15:18:30 -0400

    Main.py run/quit pygame

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+running = True
+
+while running:
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
 
 if __name__ == "__main__":
     main()
